b482d7f4daa8e9fa27957d53d70b275cf5460eb3
wuhaisheng <15624992448@163.com>
2017-09-07 16:31:24 +0800

    SpringBoot  demo

--- src/main/java/com/binggou/Application.java
+++ src/main/java/com/binggou/Application.java
@@ -11,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @MapperScan("com.binggou.dao")
 @SpringBootApplication
 public class Application {
+
     public static void main(String[] args){
         SpringApplication.run(Application.class,args);
     }
